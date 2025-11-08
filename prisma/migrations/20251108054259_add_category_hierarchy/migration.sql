832aeac8531b6023fb8ec251089dd4fbc33fd202
rmccownISoft <60158596+rmccownISoft@users.noreply.github.com>
2025-11-07 23:49:39 -0600

    feat: add support for category hierarchy

---
+++ prisma/migrations/20251108054259_add_category_hierarchy/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_categories" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "parentId" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "categories_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "categories" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_categories" ("createdAt", "id", "name", "updatedAt") SELECT "createdAt", "id", "name", "updatedAt" FROM "categories";
+DROP TABLE "categories";
+ALTER TABLE "new_categories" RENAME TO "categories";
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
